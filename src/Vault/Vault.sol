aad48d8a3addb07b226e0184261927e77c74c54a
Elli <nathan@cypherlab.org>
2025-02-04 19:34:11 +0100

    add testRedeem test

--- src/Vault/Vault.sol
+++ src/Vault/Vault.sol
@@ -130,9 +130,12 @@ contract LobsterVault is Ownable2Step, ERC4626, OpValidator {
         uint256 minAmount = uint256(bytes32(rebaseData[1:33]));
         uint256 vaultBalance = IERC20(asset()).balanceOf(address(this));
 
+        // shares owned by the owner before withdrawing. All those shares will be burnt
+        uint256 initialShares = previewWithdraw(assets);
+
         /*
         determine how many assets will be withdrawn
-        we use a minAmount because of a possible slippage happening when withdrawing and selling assets for eth from third-party contracts
+        we use a minAmount because of a possible slippage happening when withdrawing and selling tokens for assets in third-party contracts
         */
         if (vaultBalance < minAmount) {
             // vaultBalance < minAmount
@@ -146,7 +149,23 @@ contract LobsterVault is Ownable2Step, ERC4626, OpValidator {
         // else minAmount <= assets <= vaultBalance and do nothing
 
         // withdraw assets
-        return withdraw(assets, receiver, owner);
+        shares = withdraw(assets, receiver, owner);
+
+        // Ensure all the shares were burnt and there are no leftovers
+        /*
+        When withdrawing, the withdrawOperations from _verifyAndRebase can swap some tokens back to the asset. By doing so, there might be a slippage.
+        By calling withdraw, the shares are burnt depending on the asset balance that can be affected by the slippage. 
+        For instance, if the withdrawer wants to withdraw 10 assets and during withdrawOperations, we exit a UniV3 position (let's say WBTC/asset) and swap 
+        the WBTC back to asset, we would get 5 assets and x WBTC converted to 4.9 assets. The withdrawer would get 9.9 assets instead of 10 assets. when 
+        calling withdraw, we can only burn shares equivalent to 9.9 assets but the withdrawer would still have shares equivalent to 0.1 assets left in the 
+        contract. This would dilute everyone else's shares. To avoid this, we burn the leftOvers shares here.
+        */
+        uint256 leftOvers = initialShares - shares;
+        if (leftOvers > 0) {
+            _burn(owner, leftOvers);
+        }
+
+        return shares;
     }
 
     /**
@@ -161,7 +180,7 @@ contract LobsterVault is Ownable2Step, ERC4626, OpValidator {
         uint256 shares,
         address receiver,
         address owner,
-        bytes calldata rebaseData
+        bytes calldata rebaseData // todo: move the minAmount somewhere else
     ) external returns (uint256 assets) {
         // verify signature and update rebase value
         _verifyAndRebase(rebaseData);
@@ -169,6 +188,8 @@ contract LobsterVault is Ownable2Step, ERC4626, OpValidator {
         // the minimal amount of assets accepted to be withdrawn by the caller
         uint256 minAmount = uint256(bytes32(rebaseData[1:33]));
         uint256 vaultBalance = IERC20(asset()).balanceOf(address(this));
+        // the initial shares the user want to redeem
+        uint256 initialShares = shares;
 
         // preview how many assets will be withdrawn at most
         assets = previewRedeem(shares);
@@ -185,11 +206,32 @@ contract LobsterVault is Ownable2Step, ERC4626, OpValidator {
         if (vaultBalance < assets) {
             // minAmount <= vaultBalance < assets
             assets = vaultBalance;
+            shares = convertToShares(assets);
         }
         // else minAmount <= assets <= vaultBalance and do nothing
 
+        // the shares that will be used to redeem (will withdraw some assets between minAmount and assets)
+        uint256 initialOwnerShares = balanceOf(owner);
+
         // redeem shares
-        return redeem(shares, receiver, owner);
+        assets = redeem(shares, receiver, owner);
+
+        // Ensure all the shares were burnt and there are no leftovers
+        /*
+        When redeeming, the withdrawOperations from _verifyAndRebase can swap some tokens back to the asset. By doing so, there might be a slippage.
+        By calling redeem, the shares are burnt depending on the asset balance that can be affected by the slippage. 
+        For instance, if the redeemer wants to redeem 10 eth and during withdrawOperations, we exit a UniV3 position (let's say WBTC/asset) and swap 
+        the WBTC back to asset, we would get 5 assets and x WBTC converted to 4.9 assets. The redeemer would get 9.9 assets instead of 10 assets. 
+        when calling redeem, we can only burn shares equivalent to 9.9 assets but the redeemer would still have shares equivalent to 0.1 assets left 
+        in the contract. This would dilute everyone else's shares. To avoid this, we burn the leftOvers shares here.
+        */
+        uint256 leftOvers = initialShares -
+            (initialOwnerShares - balanceOf(owner));
+        if (leftOvers > 0) {
+            _burn(owner, leftOvers);
+        }
+
+        return assets;
     }
 
     /* ------------------OVERRIDE IN/OUT FUNCTIONS------------------ */
