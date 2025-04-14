e8da988915b7009d1be2961c58da3f99359fec66
Elli <nathan@cypherlab.org>
2025-04-14 17:39:47 +0200

    update in&out fees

--- test/Vault/ManagementFees.sol
+++ test/Vault/ManagementFees.sol
@@ -1,193 +1,193 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
 
-// import "forge-std/Test.sol";
-// import {PendingFeeUpdate} from "../../src/Vault/ERC4626Fees.sol";
-// import {VaultTestSetup} from "./VaultTestSetup.sol";
-// import {ERC4626Fees, IERC4626FeesEvents} from "../../src/Vault/ERC4626Fees.sol";
-// import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-// import {BASIS_POINT_SCALE} from "../../src/Vault/Constants.sol";
-// import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import "forge-std/Test.sol";
+import {PendingFeeUpdate} from "../../src/Vault/ERC4626Fees.sol";
+import {VaultTestSetup} from "./VaultTestSetup.sol";
+import {ERC4626Fees, IERC4626FeesEvents} from "../../src/Vault/ERC4626Fees.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {BASIS_POINT_SCALE} from "../../src/Vault/Constants.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-// // test deposit / withdraw / mint / redeem functions with management fee
-// // test collecting management fees manually
-// contract VaultInAndOutFeesTest is VaultTestSetup {
-//     using Math for uint256;
+// test deposit / withdraw / mint / redeem functions with management fee
+// test collecting management fees manually
+contract VaultInAndOutFeesTest is VaultTestSetup {
+    using Math for uint256;
 
-//     /* -----------------------TEST UPDATING MANAGEMENT FEE----------------------- */
-//     // todo
-//     /* -----------------------TEST MANUAL COLLECTION----------------------- */
-//     function testCollectFeesAsOwner() public {
-//         // rebase to 0
-//         rebaseVault(0, block.number + 1);
+    /* -----------------------TEST UPDATING MANAGEMENT FEE----------------------- */
+    // todo
+    /* -----------------------TEST MANUAL COLLECTION----------------------- */
+    function testCollectFeesAsOwner() public {
+        // rebase to 0
+        rebaseVault(0, block.number + 1);
 
-//         uint256 fee = 100;
-//         setManagementFeeBasisPoint(fee); // 1%
+        uint256 fee = 100;
+        setManagementFeeBasisPoint(fee); // 1%
 
-//         // alice deposit 1000 assets
-//         vm.startPrank(alice);
-//         uint256 initialFeeCollectorBalance = asset.balanceOf(
-//             vault.feeCollector()
-//         );
+        // alice deposit 1000 assets
+        vm.startPrank(alice);
+        uint256 initialFeeCollectorBalance = asset.balanceOf(
+            vault.feeCollector()
+        );
 
-//         uint256 depositAmount = 1000;
-//         uint256 shares = vault.deposit(depositAmount, alice);
-//         vm.stopPrank();
+        uint256 depositAmount = 1000;
+        uint256 shares = vault.deposit(depositAmount, alice);
+        vm.stopPrank();
 
-//         uint256 duration = 365 days;
-//         uint256 expectedFee = computeManagementFees(
-//             depositAmount,
-//             fee,
-//             duration
-//         );
+        uint256 duration = 365 days;
+        uint256 expectedFee = computeManagementFees(
+            depositAmount,
+            fee,
+            duration
+        );
 
-//         // wait for 1 year
-//         vm.warp(duration);
+        // wait for 1 year
+        vm.warp(duration);
 
-//         // collect the fees
-//         vm.startPrank(owner);
-//         uint256 collectedFees1 = vault.collectFees();
+        // collect the fees
+        vm.startPrank(owner);
+        uint256 collectedFees1 = vault.collectFees();
 
-//         // ensure fee collector shares correspond to the collected fees
-//         assertEq(vault.balanceOf(vault.feeCollector()), expectedFee);
+        // ensure fee collector shares correspond to the collected fees
+        assertEq(vault.balanceOf(vault.feeCollector()), expectedFee);
 
-//         // ensure alice's shares balance did not change
-//         assertEq(vault.balanceOf(alice), shares);
+        // ensure alice's shares balance did not change
+        assertEq(vault.balanceOf(alice), shares);
 
-//         // ensure fee collector asset balance did not change
-//         assertEq(
-//             asset.balanceOf(vault.feeCollector()),
-//             initialFeeCollectorBalance
-//         );
+        // ensure fee collector asset balance did not change
+        assertEq(
+            asset.balanceOf(vault.feeCollector()),
+            initialFeeCollectorBalance
+        );
 
-//         // ensure collectedFees1 is accurate
-//         assertEq(
-//             vault.balanceOf(vault.feeCollector()) - initialFeeCollectorBalance,
-//             collectedFees1
-//         );
+        // ensure collectedFees1 is accurate
+        assertEq(
+            vault.balanceOf(vault.feeCollector()) - initialFeeCollectorBalance,
+            collectedFees1
+        );
 
-//         // ensure we can't withdraw again in the same block
-//         uint256 vaultTotalSupplyBefore = vault.totalSupply();
-//         uint256 collectedFees2 = vault.collectFees();
+        // ensure we can't withdraw again in the same block
+        uint256 vaultTotalSupplyBefore = vault.totalSupply();
+        uint256 collectedFees2 = vault.collectFees();
 
-//         // ensure collectedFees2 is 0
-//         assertEq(collectedFees2, 0);
+        // ensure collectedFees2 is 0
+        assertEq(collectedFees2, 0);
 
-//         // ensure no shares were minted
-//         assertEq(vaultTotalSupplyBefore, vault.totalSupply());
+        // ensure no shares were minted
+        assertEq(vaultTotalSupplyBefore, vault.totalSupply());
 
-//         // ensure alice's shares balance did not change
-//         assertEq(vault.balanceOf(alice), shares);
+        // ensure alice's shares balance did not change
+        assertEq(vault.balanceOf(alice), shares);
 
-//         // ensure fee collector asset balance did not change
-//         assertEq(
-//             asset.balanceOf(vault.feeCollector()),
-//             initialFeeCollectorBalance
-//         );
+        // ensure fee collector asset balance did not change
+        assertEq(
+            asset.balanceOf(vault.feeCollector()),
+            initialFeeCollectorBalance
+        );
 
-//         vm.stopPrank();
-//     }
+        vm.stopPrank();
+    }
 
-//     /* -----------------------TEST DEPOSIT / MINT / WITHDRAW / REDEEM----------------------- */
-//     function testDeposit() public {
-//         // rebase to 0
-//         rebaseVault(0, block.number + 1);
+    /* -----------------------TEST DEPOSIT / MINT / WITHDRAW / REDEEM----------------------- */
+    function testDeposit() public {
+        // rebase to 0
+        rebaseVault(0, block.number + 1);
 
-//         uint256 initialFeeCollectorBalance = asset.balanceOf(
-//             vault.feeCollector()
-//         );
+        uint256 initialFeeCollectorBalance = asset.balanceOf(
+            vault.feeCollector()
+        );
 
-//         // alice deposit 1000 assets
-//         vm.startPrank(alice);
-//         uint256 depositAmount = 1000;
-//         uint256 shares = vault.deposit(depositAmount, alice);
-//         vm.stopPrank();
+        // alice deposit 1000 assets
+        vm.startPrank(alice);
+        uint256 depositAmount = 1000;
+        uint256 shares = vault.deposit(depositAmount, alice);
+        vm.stopPrank();
 
-//         uint256 fee = 100;
-//         setManagementFeeBasisPoint(fee); // 1%
+        uint256 fee = 100;
+        setManagementFeeBasisPoint(fee); // 1%
 
-//         uint256 feeInforcementTimestamp = block.timestamp;
+        uint256 feeInforcementTimestamp = block.timestamp;
 
-//         uint256 duration = 365 days;
-//         uint256 expectedFee = computeManagementFees(shares, fee, duration);
+        uint256 duration = 365 days;
+        uint256 expectedFee = computeManagementFees(shares, fee, duration);
 
-//         // go 1 year in the future
-//         vm.warp(feeInforcementTimestamp + duration);
+        // go 1 year in the future
+        vm.warp(feeInforcementTimestamp + duration);
 
-//         // bob deposit
-//         vm.startPrank(bob);
-//         uint256 bobDeposit = 1000;
-//         uint256 bobShares = vault.deposit(bobDeposit, bob);
-//         vm.stopPrank();
+        // bob deposit
+        vm.startPrank(bob);
+        uint256 bobDeposit = 1000;
+        uint256 bobShares = vault.deposit(bobDeposit, bob);
+        vm.stopPrank();
 
-//         // management fees must have been collected
-//         assertEq(
-//             vault.balanceOf(vault.feeCollector()),
-//             initialFeeCollectorBalance + expectedFee
-//         );
+        // management fees must have been collected
+        assertEq(
+            vault.balanceOf(vault.feeCollector()),
+            initialFeeCollectorBalance + expectedFee
+        );
 
-//         // ensure no assets where moved out of the vault
-//         assertEq(asset.balanceOf(address(vault)), depositAmount + bobDeposit);
+        // ensure no assets where moved out of the vault
+        assertEq(asset.balanceOf(address(vault)), depositAmount + bobDeposit);
 
-//         // ensure alice can withdraw her deposit - fee
-//         assertEq(vault.maxWithdraw(alice), vault.convertToAssets(shares));
+        // ensure alice can withdraw her deposit - fee
+        assertEq(vault.maxWithdraw(alice), vault.convertToAssets(shares));
 
-//         // ensure bob can withdraw his deposit
-//         assertEq(vault.maxWithdraw(bob), vault.convertToAssets(bobShares));
+        // ensure bob can withdraw his deposit
+        assertEq(vault.maxWithdraw(bob), vault.convertToAssets(bobShares));
 
-//         // ensure alice shares did not change
-//         assertEq(vault.balanceOf(alice), shares);
-//     }
-//     function testMint() public {
-//         // rebase to 0
-//         rebaseVault(0, block.number + 1);
+        // ensure alice shares did not change
+        assertEq(vault.balanceOf(alice), shares);
+    }
+    function testMint() public {
+        // rebase to 0
+        rebaseVault(0, block.number + 1);
 
-//         uint256 initialFeeCollectorBalance = asset.balanceOf(
-//             vault.feeCollector()
-//         );
+        uint256 initialFeeCollectorBalance = asset.balanceOf(
+            vault.feeCollector()
+        );
 
-//         // alice mint 1000 shares
-//         vm.startPrank(alice);
-//         uint256 mintAmount = 1000;
-//         uint256 assets = vault.mint(mintAmount, alice);
-//         vm.stopPrank();
+        // alice mint 1000 shares
+        vm.startPrank(alice);
+        uint256 mintAmount = 1000;
+        uint256 assets = vault.mint(mintAmount, alice);
+        vm.stopPrank();
 
-//         uint256 fee = 100;
-//         setManagementFeeBasisPoint(fee); // 1%
+        uint256 fee = 100;
+        setManagementFeeBasisPoint(fee); // 1%
 
-//         uint256 feeInforcementTimestamp = block.timestamp;
+        uint256 feeInforcementTimestamp = block.timestamp;
 
-//         uint256 duration = 365 days;
-//         uint256 expectedFee = computeManagementFees(mintAmount, fee, duration);
+        uint256 duration = 365 days;
+        uint256 expectedFee = computeManagementFees(mintAmount, fee, duration);
 
-//         // go 1 year in the future
-//         vm.warp(feeInforcementTimestamp + duration);
+        // go 1 year in the future
+        vm.warp(feeInforcementTimestamp + duration);
 
-//         // bob mint
-//         vm.startPrank(bob);
-//         uint256 bobMint = 1000;
-//         uint256 bobAssets = vault.mint(bobMint, bob);
-//         vm.stopPrank();
+        // bob mint
+        vm.startPrank(bob);
+        uint256 bobMint = 1000;
+        uint256 bobAssets = vault.mint(bobMint, bob);
+        vm.stopPrank();
 
-//         // management fees must have been collected
-//         assertEq(
-//             vault.balanceOf(vault.feeCollector()),
-//             initialFeeCollectorBalance + expectedFee
-//         );
+        // management fees must have been collected
+        assertEq(
+            vault.balanceOf(vault.feeCollector()),
+            initialFeeCollectorBalance + expectedFee
+        );
 
-//         // ensure no assets where moved out of the vault
-//         assertEq(asset.balanceOf(address(vault)), assets + bobAssets);
+        // ensure no assets where moved out of the vault
+        assertEq(asset.balanceOf(address(vault)), assets + bobAssets);
 
-//         // ensure alice can redeem her shares - fee
-//         assertEq(vault.maxWithdraw(alice), assets - vault.convertToShares(expectedFee));
+        // ensure alice can redeem her shares - fee
+        assertEq(vault.maxWithdraw(alice), assets - vault.convertToShares(expectedFee));
 
-//         // ensure bob can redeem his shares
-//         assertEq(vault.maxRedeem(bob), vault.convertToShares(bobAssets));
+        // ensure bob can redeem his shares
+        assertEq(vault.maxRedeem(bob), vault.convertToShares(bobAssets));
 
-//         // // ensure alice shares did not change
-//         assertEq(vault.balanceOf(alice), mintAmount);
-//     }
+        // // ensure alice shares did not change
+        assertEq(vault.balanceOf(alice), mintAmount);
+    }
 
 
-//     // todo
-// }
+    // todo
+}
