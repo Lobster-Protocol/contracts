67a0d236e9d55567b01902b478ea50b03c2edc99
Elli <nathan@cypherlab.org>
2025-04-28 10:15:50 +0200

    set sigenrs as contract constants

--- test/Modules/OpValidators/GenericMusigOpValidatorTestSetup.sol
+++ test/Modules/OpValidators/GenericMusigOpValidatorTestSetup.sol
@@ -14,10 +14,10 @@ contract GenericMusigOpValidatorTestSetup is Test {
     Counter public counter;
 
     // anvil private keys
-    uint256 public signer1 = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-    uint256 public signer2 = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
-    uint256 public signer3 = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
-    uint256 public notValidatorSigner = 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
+    uint256 public constant signer1 = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 public constant signer2 = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
+    uint256 public constant signer3 = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
+    uint256 public constant notValidatorSigner = 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
     Signer[] private signers;
 
     function setUp() public {
